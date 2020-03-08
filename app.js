5715405ea8cc83979d60617613df75d8fbcc1980
Kevin Stradtman <stradtkt22@gmail.com>
2020-03-07 22:28:59 -0500

    Added a new item

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-
+//Item Controller
 const ItemCtrl = (function() {
     //Item Constructor
     const Item = function(id, name, calories) {
@@ -23,15 +23,36 @@ const ItemCtrl = (function() {
         getItems: function() {
             return data.items;
         },
+        addItem: function(name, calories) {
+            let ID;
+            //create id
+            if(data.items.length > 0) {
+                ID = data.items[data.items.length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            //calories to number
+            const calories = parseInt(calories);
+            //create new item
+            newItem = new Item(ID, name, calories);
+            //add to items array
+            data.items.push(newItem);
+            return newItem;
+        },
         logData: function() {
             return data;
         }
     }
 })();
 
+
+//UI Controller
 const UICtrl = (function() {
     const UISelectors = {
-        itemList: '#item-list'
+        itemList: '#item-list',
+        addBtn: '.add-btn',
+        itemName: '#item-name',
+        itemCalories: '#item-calories'
     }
     return {
         populateItemList: function(items) {
@@ -40,18 +61,50 @@ const UICtrl = (function() {
                 html += `<li class="collection-item" id="item-${item.id}"><strong>${item.name}: </strong><em>${item.calories}</em> <a href="#" class="secondary-content"><i class="edit-item fas fa-pen-square"></i></a></li>`;
             });
             document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+        getItemInput: function() {
+            return {
+                name: document.querySelector(UISelectors.itemName).value,
+                calories: document.querySelector(UISelectors.itemCalories).value
+            }
+        },
+        getSelectors: function() {
+            return UISelectors;
         }
     }
 
 })();
 
+
+//App Controller
 const App = (function(ItemCtrl, UICtrl) {
+
+    const loadEventListeners = function() {
+        //Get UI Selectors
+        const UISelectors = UICtrl.getSelectors();
+        //Add item event
+        document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+    }
+
+    const itemAddSubmit = function(e) {
+        const input = UICtrl.getItemInput();
+        
+        if(input.name !== '' && input.calories !== '') {
+            //Add item
+            const newItem = ItemCtrl.addItem(input.name, input.calories);
+
+        }
+        e.preventDefault();
+    }
+
     return {
         init: function() {
             // console.log('Initializing App...');
             const items = ItemCtrl.getItems();
             // console.log(items);
             UICtrl.populateItemList(items);
+            //load event listeners 
+            loadEventListeners();
         }
     }
 })(ItemCtrl, UICtrl);
