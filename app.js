19d32d07ed0c09469456d1f7c23317bea5ce16c0
Kevin Stradtman <stradtkt22@gmail.com>
2020-03-08 19:24:40 -0400

    Added total calories

--- app.js
+++ app.js
@@ -9,11 +9,19 @@ const ItemCtrl = (function(){
       this.name = name;
       this.calories = calories;
     }
+  
+    // Data Structure / State
     const data = {
-      items: [],
+      items: [
+        // {id: 0, name: 'Steak Dinner', calories: 1200},
+        // {id: 1, name: 'Cookie', calories: 400},
+        // {id: 2, name: 'Eggs', calories: 300}
+      ],
       currentItem: null,
       totalCalories: 0
     }
+  
+    // Public methods
     return {
       getItems: function(){
         return data.items;
@@ -26,13 +34,31 @@ const ItemCtrl = (function(){
         } else {
           ID = 0;
         }
+  
         // Calories to number
         calories = parseInt(calories);
+  
         // Create new item
         newItem = new Item(ID, name, calories);
+  
         // Add to items array
         data.items.push(newItem);
+  
         return newItem;
+      },
+      getTotalCalories: function(){
+        let total = 0;
+  
+        // Loop through items and add cals
+        data.items.forEach(function(item){
+          total += item.calories;
+        });
+  
+        // Set total cal in data structure
+        data.totalCalories = total;
+  
+        // Return total
+        return data.totalCalories;
       },
       logData: function(){
         return data;
@@ -41,13 +67,15 @@ const ItemCtrl = (function(){
   })();
   
   
+  
   // UI Controller
   const UICtrl = (function(){
     const UISelectors = {
       itemList: '#item-list',
       addBtn: '.add-btn',
       itemNameInput: '#item-name',
-      itemCaloriesInput: '#item-calories'
+      itemCaloriesInput: '#item-calories',
+      totalCalories: '.total-calories'
     }
     
     // Public methods
@@ -97,42 +125,59 @@ const ItemCtrl = (function(){
       hideList: function(){
         document.querySelector(UISelectors.itemList).style.display = 'none';
       },
+      showTotalCalories: function(totalCalories){
+        document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
+      },
       getSelectors: function(){
         return UISelectors;
       }
     }
   })();
-
-
-
+  
+  
+  
   // App Controller
   const App = (function(ItemCtrl, UICtrl){
     // Load event listeners
     const loadEventListeners = function(){
       // Get UI selectors
       const UISelectors = UICtrl.getSelectors();
+  
       // Add item event
       document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
     }
+  
     // Add item submit
     const itemAddSubmit = function(e){
       // Get form input from UI Controller
       const input = UICtrl.getItemInput();
+  
       // Check for name and calorie input
       if(input.name !== '' && input.calories !== ''){
         // Add item
         const newItem = ItemCtrl.addItem(input.name, input.calories);
+  
         // Add item to UI list
         UICtrl.addListItem(newItem);
+  
+        // Get total calories
+        const totalCalories = ItemCtrl.getTotalCalories();
+        // Add total calories to UI
+        UICtrl.showTotalCalories(totalCalories);
+  
         // Clear fields
         UICtrl.clearInput();
       }
+  
       e.preventDefault();
     }
+  
+    // Public methods
     return {
       init: function(){
         // Fetch items from data structure
-        const items = ItemCtrl.getItems();  
+        const items = ItemCtrl.getItems();
+  
         // Check if any items
         if(items.length === 0){
           UICtrl.hideList();
@@ -140,8 +185,18 @@ const ItemCtrl = (function(){
           // Populate list with items
           UICtrl.populateItemList(items);
         }
+  
+        // Get total calories
+        const totalCalories = ItemCtrl.getTotalCalories();
+        // Add total calories to UI
+        UICtrl.showTotalCalories(totalCalories);
+  
+        // Load event listeners
         loadEventListeners();
       }
     }
+    
   })(ItemCtrl, UICtrl);
+  
+  // Initialize App
   App.init();
